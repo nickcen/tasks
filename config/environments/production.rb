8854636550fd060572b176c15775192a85c4522f
CenYonghong <cenyongh@gmail.com>
2014-02-05 03:09:45 +0800

    ~

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Tasks::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.host = 'taskse.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'taskse.herokuapp.com', :port => '80' }
 end
